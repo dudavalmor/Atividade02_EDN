6542699d511da8ed103f2dd8dd65435e0b268f3f
Eduarda Valmor <eduarda.bserrano@gmail.com>
2025-05-25 22:06:28 -0300

    Consumo de combustível

---
+++ consumoCombustivel.py
@@ -0,0 +1,10 @@
+distanciaPercorrida = 300;
+combustivelGasto = 25;
+
+consumoMedio = distanciaPercorrida / combustivelGasto;
+
+print("----- Resumo da viagem -----");
+print(f"Distância Percorrida: {distanciaPercorrida} km");
+print(f"Combustível Gasto: {combustivelGasto} litros");
+print(f"Consumo Médio: {consumoMedio:.2f} km/l");
+print("-------------------------------");
