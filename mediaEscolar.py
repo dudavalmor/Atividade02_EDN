a49b5cbf6750a625884133375b6790cab5b4c1c2
Eduarda Valmor <eduarda.bserrano@gmail.com>
2025-05-25 22:03:58 -0300

    Cáluclo Média Escolar

---
+++ mediaEscolar.py
@@ -0,0 +1,12 @@
+nota1 = 7.5;
+nota2 = 8.0;
+nota3 = 6.5;
+
+media = (nota1 + nota2 + nota3) / 3;
+
+print("----- Resumo da Média Escolar -----");
+print(f"Nota 1: {nota1:.1f}");
+print(f"Nota 2: {nota2:.1f}");
+print(f"Nota 3: {nota3:.1f}");
+print(f"Média: {media:.2f}");
+print("-------------------------------------");
